00a74e83cf4459b756ed81814d74e4059bba8e42
di3g019 <tumasterdiego1@gmail.com>
2024-12-05 18:36:35 -0500

    RegistroBeta

--- app/src/main/java/com/example/msk/MainActivity.kt
+++ app/src/main/java/com/example/msk/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.example.msk
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // Encuentra el botón
+        val btnIngresar = findViewById<Button>(R.id.btnIngresar)
+
+        // Configura el clic del botón
+        btnIngresar.setOnClickListener {
+            // Crea un Intent para ir a MenuActivity
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)  // Inicia la actividad
+        }
     }
 }
---
+++ app/src/main/java/com/example/msk/MenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.msk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu) // menu es el nombre de mi archivo .xml
+    }
+}
